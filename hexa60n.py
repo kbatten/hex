33ecc69f2697fb6298ae32b73fab2fd794a73547
Keith Batten <kbatten@gmail.com>
2018-07-07 19:16:30 -0700

    calculate line intersections

--- hexa60n.py
+++ hexa60n.py
@@ -27,6 +27,20 @@ def hexagon_poly(dwg, x, y, m):
     return dwg.polyline(points=hexagon(x, y, m))
 
 
+def intersect(p1, p2):
+    '''
+    calculates a specific intersection of 45 degree lines
+     the first coord is on the line going y+
+     the second coord is the line going y-
+    '''
+    b1 = p1[1] - p1[0]
+    b2 = p2[1] + p2[0]
+
+    x = (b2 - b1) / 2
+    y = x + b1
+    return (x, y)
+
+
 def hexes(image):
     '''
     draw 5 hexes
@@ -61,6 +75,7 @@ def hexes(image):
     # x = 37.5
     # y = 87.5
 
+
     # coordinates are ccw starting at the top (lowest y value)
     h0 = [(45, 0), (25, 20), (25, 60), (45, 80), (65, 60), (65, 20), (45, 0)]
     h1 = [(95, 0), (75, 20), (75, 60), (95, 80), (115, 60), (115, 20), (95, 0)]
@@ -68,9 +83,14 @@ def hexes(image):
     h3 = [(70, 70), (50, 90), (50, 130), (70, 150), (90, 130), (90, 90), (70, 70)]
     h4 = [(120, 70), (100, 90), (100, 130), (120, 150), (140, 130), (140, 90), (120, 70)]
 
-    # h0-0, h0-1, h0-2, h0-3, (37.5, 87.5), h2-0, h2-1, h2-2, h2-3, h2-4, h3-2 ...
-    lines = [h0[0], h0[1], h0[2], h0[3], (37.5, 87.5), h2[0], h2[1], h2[2], h2[3], h2[4], h3[2]]
-    hexes.add(image.polyline(points=lines))
+    inter1 = intersect(h2[0], h0[3])
+    inter2 = intersect(h1[2], h3[0])
+
+    lines = [h0[0], h0[1], h0[2], h0[3], inter1, h2[0], h2[1], h2[2], h2[3], h2[4],
+             h3[2], h3[3], h3[4], h3[5], h3[0],
+             inter2, h1[3], h1[4], h1[5], h1[0], h1[1], h0[5], h0[0]]
+    hexes.add(image.polygon(points=lines))
+    hexes.add(image.polygon(points=h4))
 
 
 def main():
